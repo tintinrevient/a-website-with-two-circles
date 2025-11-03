ede6b7a4de33c04157a38783ac7ecbfc7de1996d
tintinrevient <zhaobenben007@googlemail.com>
2025-11-03 22:49:43 +0100

    Remove base path

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,6 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
   kit: {
     adapter: adapter(),
-    paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-    },
   },
 };
 
